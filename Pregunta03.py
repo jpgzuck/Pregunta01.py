c3728ddf4b5f19a94c87ab4ad5cd02e5060a2c50
jpgzuck <50914051+jpgzuck@users.noreply.github.com>
2019-06-25 08:27:10 -0500

    Add files via upload

---
+++ Pregunta03.py
@@ -0,0 +1,23 @@
+a = []
+i=0
+while len(a)<6:
+    i=i+1
+    n = int(input("Ingrese número: "))
+    a.append(n)
+    
+N = a
+print(N)
+
+import random
+b = []
+i=0
+while len(b)<6:
+    i=i+1
+    m = random.randint(1,10)
+    b.append(m)
+M = b
+print(M)
+
+print("Ha ganado 6 millones de soles")
+print("Ha ganado 100 mil soles")
+print("Siga intentando")
