4e5022c489b22703a166a6c7a67cb81362613771
jpgzuck <50914051+jpgzuck@users.noreply.github.com>
2019-06-25 08:25:26 -0500

    Add files via upload

---
+++ Pregunta01.py
@@ -0,0 +1,31 @@
+matriz1 = [(2,2,4),(2,1,1),(3,3,4)]
+matriz2 = [(1,2,3),(4,1,4),(3,1,2)]
+matriz3 = [(4,2,3),(2,3,4),(1,3,3)]
+
+N1_1_1 = matriz1[0][0]
+N1_2_2 = matriz1[1][1]
+N1_3_3 = matriz1[2][2]
+suma1= N1_1_1 + N1_2_2 + N1_3_3
+
+print (matriz1)
+print ("La suma es: ",suma1)
+
+N2_1_1 = matriz2[0][0]
+N2_2_2 = matriz2[1][1]
+N2_3_3 = matriz2[2][2]
+suma= N2_1_1 + N1_2_2 + N1_3_3
+
+suma2= N2_1_1 + N2_2_2 + N2_3_3
+
+print (matriz2)
+print ("La suma es: ",suma2)
+
+N3_1_1 = matriz3[0][0]
+N3_2_2 = matriz3[1][1]
+N3_3_3 = matriz3[2][2]
+suma= N3_1_1 + N3_2_2 + N3_3_3
+
+suma3= N3_1_1 + N3_2_2 + N3_3_3
+
+print (matriz3)
+print ("La suma es: ",suma3)
